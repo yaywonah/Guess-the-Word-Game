597a8cd692cf592f02ce74d997470ef4d422a22c
yewon_s <seoyewon93@gmail.com>
2023-05-19 16:21:56 -0400

    Accept and Validate Player Guesses

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const buttonPlayAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const circlePlaceholder = function (word) {
      const placeholderWord = [];
@@ -22,7 +23,38 @@ circlePlaceholder(word);
 
 buttonGuess.addEventListener("click", function (e) {
     e.preventDefault();
-    const input = inputField.value;
-    console.log(input);
+
+    message.innerText = "";
+
+    const guess = inputField.value;
+
+    const submit = validateInput(guess);
+    if (submit) {
+        makeGuess(guess);
+    }
+
     inputField.value = "";
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = `Please enter a letter.`;
+    } else if (input.length > 1) {
+        message.innerText = `Please enter a single letter.`;
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = `Please enter a letter from A to Z`;
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes((guess))) {
+        message.innerText = 'You have already guessed that letter. Try again!';
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+}
